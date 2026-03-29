1d981fd3b03df61f821b85aaf09bc8b059d3a2e1
jxh090 <jaden.huang@insigniafinancial.com.au>
2026-03-30 09:55:05 +1100

    fix: remove frontend-lit

--- frontend-lit/src/d3.d.ts
+++
@@ -1,7 +0,0 @@
-// d3.d.ts
-declare module "https://cdn.jsdelivr.net/npm/d3@7/+esm" {
-  export function scaleLinear(): any;
-  export function select(element: any): any;
-  export function axisBottom(scale: any): any;
-  // ... more D3 types
-}
